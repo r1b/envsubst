807d7cbe663400eb2a4e6e58470b51f113708272
Robert C Jensen <robert.cole.jensen@gmail.com>
2018-11-24 19:20:35 -0500

    dont repeatedly set variables

--- bin/envsubst.scm
+++ bin/envsubst.scm
@@ -1,11 +1,14 @@
 (use envsubst)
 
+(define (parse-lines)
+  (let ((line (read-line)))
+    (unless (eof-object? line)
+      (begin (write-line (parse-line line))
+             (parse-lines)
+             (exit)))))
+
 (define (main args)
   (begin
     (unless (null? args)
       (set-variables! args))
-    (let ((line (read-line)))
-      (unless (eof-object? line)
-        (begin (write-line (parse-line line))
-               (main _)
-               (exit))))))
+    (parse-lines)))
